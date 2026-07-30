9fc208765d647e8a793f261f951db9460490ed93
rehaannazir <rehaan689nazir@gmail.com>
2026-07-30 20:11:58 +0500

    lead extractor

---
+++ 09-Extractors/lead_extractor.py
@@ -0,0 +1,68 @@
+from pydantic import BaseModel, Field
+from typing import Optional, Literal
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import ChatPromptTemplate
+
+# Loading env variables
+load_dotenv()
+
+
+# Required output schema
+class Lead(BaseModel):
+
+    name: str = Field(description="Full name of the person contacting the company")
+    company: str = Field(description="Name of the company the person represents")
+    intent: str = Field(
+        description="What the person wants (e.g. any service, demo, support, meeting or anything else means purpose of message)"
+    )
+    budget_signal: Optional[str] = Field(
+        default=None, description="Any mention of budget or spend"
+    )
+    urgency: Literal["Normal", "Urgent", "Very Urgent"] = Field(
+        description="Urgency inferred from tone/wording"
+    )
+
+
+# Preparing the structured LLM
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash", response_mime_type="application/json"
+)
+structured_llm = llm.with_structured_output(Lead)
+
+# Making the Prompt Runnable
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are receptionist and CRM manager of a company. Just extract  name, company, intent, budget(if given), urgency(from msg intensity and emotions). Return only JSON, nothing else.",
+        ),
+        (
+            "human",
+            "The message of the person:- message : {email}. Now fetch valuable insights from it required by the company to fill CRM.",
+        ),
+    ]
+)
+
+print(
+    """╔══════════════════════════════════════════════════════════════════════════════╗
+║                                                                              ║
+║   ███╗   ██╗███████╗██╗  ██╗ █████╗ ██████╗  █████╗                          ║
+║   ████╗  ██║██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔══██╗                         ║
+║   ██╔██╗ ██║█████╗   ╚███╔╝ ███████║██████╔╝███████║                         ║
+║   ██║╚██╗██║██╔══╝   ██╔██╗ ██╔══██║██╔══██╗██╔══██║                         ║
+║   ██║ ╚████║███████╗██╔╝ ██╗██║  ██║██║  ██║██║  ██║                         ║
+║   ╚═╝  ╚═══╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝                         ║
+║                                                                              ║
+║      AI Automation • Agents • RAG • LangGraph                                ║ 
+║                                                                              ║
+╚══════════════════════════════════════════════════════════════════════════════╝"""
+)
+
+email = input("\nHow can we help you?\n")
+
+chain = prompt | structured_llm
+
+answer = chain.invoke({"email": email})
+
+print(f"\n\n {answer}")
